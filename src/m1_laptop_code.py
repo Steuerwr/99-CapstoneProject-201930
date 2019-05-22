850d9a16c3921b53951fbda805bb554a27f30082
steuerwr <steuerwr@rose-hulman.edu>
2019-05-22 11:58:49 -0400

    robot number

--- src/m1_laptop_code.py
+++ src/m1_laptop_code.py
@@ -10,6 +10,7 @@
 
 import tkinter
 from tkinter import ttk
+import math
 
 import mqtt_remote_method_calls as mqtt
 import m2_laptop_code as m2
@@ -24,25 +25,55 @@ def get_my_frame(root, window, mqtt_sender):
     # Done 2: Put your name in the above.
 
     # Add the rest of your GUI to your frame:
-    # TODO: Put your GUI onto your frame (using sub-frames if you wish).
-    forward_distance_button = ttk.Button(frame, text="Forward Distance")
-    forward_distance_button.grid()
-    forward_distance_button["command"] = lambda: handle_forward_distance(
-        speed_entry, distance_entry, mqtt_sender)
+    # : Put your GUI onto your frame (using sub-frames if you wish).
+    #forward_distance_button = ttk.Button(frame, text="Forward Distance")
+    #forward_distance_button.grid()
+    #forward_distance_button["command"] = lambda: MyLaptopDelegate.handle_forward_distance(
+     #   speed_entry, distance_entry, mqtt_sender)
 
-    distance_entry = ttk.Entry(frame)
-    distance_entry.insert(0, "100")
-    distance_entry.grid()
+    #distance_entry = ttk.Entry(frame)
+    #distance_entry.insert(0, "100")
+    #distance_entry.grid()
 
-    speed_button = ttk.Button(frame, text="Speed")
-    speed_button.grid()
-    speed_entry = ttk.Entry(frame)
-    speed_entry.insert(0, "100")
+    #speed_button = ttk.Button(frame, text="Speed")
+    #speed_button.grid()
+    #speed_entry = ttk.Entry(frame)
+    #speed_entry.insert(0, "100")
+    #speed_entry.grid()
+
+    direction_label = ttk.Label(frame, text="Choose a direction: ")
+    direction_label.grid()
+
+    forwards_button = ttk.Button(frame, text="Forwards")
+    forwards_button.grid(row=2, column=0)
+
+    backwards_button = ttk.Button(frame, text="Backwards")
+    backwards_button.grid(row=2, column=1)
+
+    speed_label = ttk.Label(frame, text='Enter speed')
+    speed_label.grid()
+    speed_entry = ttk.Entry(frame, width=8)
     speed_entry.grid()
 
+    distance_label = ttk.Label(frame, text='Enter distance')
+    distance_label.grid()
+    distance_box = ttk.Entry(frame, width=8)
+    distance_box.grid()
+
+    function_label = ttk.Label(frame, text='Move until...')
+    function_button = ttk.Button(frame, text='Move')
+    frame_label.grid()
+    function_label.grid()
+    function_button.grid()
+
+    forwards_button["command"] = lambda: MyLaptopDelegate.forward(speed_entry, distance_box, mqtt_sender)
+    backwards_button["command"] = lambda: MyLaptopDelegate.backward(speed_entry, distance_box, mqtt_sender)
+    function_button["command"] = lambda: MyLaptopDelegate.move_until(speed_entry, distance_box, mqtt_sender)
 
     # Return your frame:
     return frame
+
+
 
 
 class MyLaptopDelegate(object):
@@ -58,18 +89,20 @@ class MyLaptopDelegate(object):
     def set_mqtt_sender(self, mqtt_sender):
         self.mqtt_sender = mqtt_sender
 
-def go(mqtt_sender, direction, left_wheel_speed, right_wheel_speed):
-    print()
-    print("Sending a message to the robot to", direction)
-    print("  using wheel motor speeds:", left_wheel_speed, right_wheel_speed)
-    mqtt_sender.send_message("go", [left_wheel_speed, right_wheel_speed])
+    def forward(speed_entry, distance_box, mqtt_sender):
+        speed = speed_entry.get()
+        distance = distance_box.get()
+        mqtt_sender.send_message("move_forward", [speed, distance])
 
-def handle_forward_distance(speed_entry_box, distance_entry_box, mqtt_sender):
-    left = int(speed_entry_box.get())
-    right = int(speed_entry_box.get())
-    inches = int(distance_entry_box.get())
-    go(mqtt_sender, "FORWARD", left, right)
+    def backward(speed_entry, distance_box, mqtt_sender):
+        speed = speed_entry.get()
+        distance = distance_box.get()
+        mqtt_sender.send_message("move_backward", [speed, distance])
 
+    def move_until(speed_entry, distance_box, mqtt_sender):
+        speed = speed_entry.get()
+        distance = distance_box.get()
+        mqtt_sender.send_message("move_until", [speed, distance])
     # TODO: Add methods here as needed.
 
 
--- src/m1_robot_code.py
+++ src/m1_robot_code.py
@@ -8,9 +8,10 @@
 
 import mqtt_remote_method_calls as mqtt
 import rosebot
+import m1_laptop_code as m1
 import m2_robot_code as m2
 import m3_robot_code as m3
-
+import math
 
 class MyRobotDelegate(object):
     """
@@ -31,10 +32,50 @@ class MyRobotDelegate(object):
         print_message_received("go", [left_motor_speed, right_motor_speed])
         self.robot.drive_system.go(left_motor_speed, right_motor_speed)
 
-    def forward(self, speed, inches):
-        self.robot.drive_system.go(speed,speed)
+    def forward(self, speed, distance):
+        self.robot.drive_system.right_motor.reset_position()
+        self.go(int(speed), int(speed))
         while True:
-            if self.robot
+            if self.robot.drive_system.right_motor.get_position() >= (88.149 * int(distance)):
+                self.robot.drive_system.stop()
+                break
+
+    def backward(self, speed, distance):
+        self.robot.drive_system.right_motor.reset_position()
+        self.go(-int(speed), -int(speed))
+        while True:
+            if abs(self.robot.drive_system.right_motor.get_position()) >= (88.149 * int(distance)):
+                self.robot.drive_system.stop()
+                break
+
+    def get_distance(self):
+        dist = []
+        max = 0
+        sum = 0
+        for k in range(5):
+            dist = dist + [self.robot.sensor_system.ir_proximity_sensor.get_distance()]
+        for k in range(len(dist)):
+            if dist[k] > max:
+                max = dist[k]
+        min = dist[0]
+        for k in range(len(dist)):
+            if dist[k] < min:
+                min = dist[k]
+        for k in range(len(dist)):
+            sum = sum + dist[k]
+        sum = (sum - (max + min)) / 3
+        return sum
+
+    def move_until(self, speed, distance):
+        self.go(int(speed), int(speed))
+        while True:
+            dist = self.get_distance()
+            print(dist)
+            if dist < int(distance):
+                self.robot.drive_system.right_motor.turn_off()
+                self.robot.drive_system.left_motor.turn_off()
+                break
+
     # TODO: Add methods here as needed.
 
 
